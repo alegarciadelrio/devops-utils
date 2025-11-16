a0c6b4b4d227c7e14d72361666a4eae43e42bc9d
Alejandro Garcia <ale.garciadelrio@gmail.com>
2025-11-16 18:24:23 -0300

    feat: add workstation setup script.

---
+++ workstation/workstation.sh
@@ -0,0 +1,109 @@
+#!/bin/bash
+sudo apt update
+sudo apt upgrade 
+sudo apt install nmap
+sudo apt install curl
+sudo apt install solaar vim
+sudo apt install remmina
+
+sudo snap connect remmina:audio-record :audio-record
+sudo snap connect remmina:avahi-observe :avahi-observe
+sudo snap connect remmina:cups-control :cups-control
+sudo snap connect remmina:mount-observe :mount-observe
+sudo snap connect remmina:password-manager-service :password-manager-service
+sudo snap connect remmina:ssh-keys :ssh-keys
+sudo snap connect remmina:ssh-public-keys :ssh-public-keys 
+
+sudo apt install gnome-shell-extension-gsconnect
+sudo apt install indicator-multiload
+sudo apt install python3-venv
+sudo ln -s /usr/bin/python3 /usr/bin/python
+
+sudo apt update
+sudo apt install libxcb-render-util0
+sudo apt install libxcb-keysyms1
+sudo apt install libxcb-image0
+sudo apt install libxcb-icccm4
+wget https://download.mikrotik.com/routeros/winbox/4.0beta41/WinBox_Linux.zip
+unzip WinBox_Linux.zip
+
+# install solar 
+sudo add-apt-repository ppa:solaar-unifying/stable
+sudo apt update
+sudo apt install solaar 
+cd /etc/udev/rules.d 
+sudo curl -O https://raw.githubusercontent.com/pwr-Solaar/Solaar/master/rules.d-uinput/42-logitech-unify-permissions.rules 
+cd
+
+# Download and install nvm:
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+# in lieu of restarting the shell
+\. "$HOME/.nvm/nvm.sh"
+# Download and install Node.js:
+nvm install 24
+# Verify the Node.js version:
+node -v # Should print "v24.11.1".
+# Verify npm version:
+npm -v # Should print "11.6.2".
+
+
+# terraform
+sudo apt-get update && sudo apt-get install -y gnupg software-properties-common
+wget -O- https://apt.releases.hashicorp.com/gpg | \
+gpg --dearmor | \
+sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
+gpg --no-default-keyring \
+--keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
+--fingerprint
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(grep -oP '(?<=UBUNTU_CODENAME=).*' /etc/os-release || lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update
+sudo apt-get install terraform
+
+# l2tp
+sudo add-apt-repository ppa:nm-l2tp/network-manager-l2tp
+sudo apt-get install network-manager-l2tp network-manager-l2tp-gnome
+
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin 
+sudo docker run hello-world
+sudo groupadd docker
+sudo usermod -aG docker $USER 
+newgrp docker
+
+# windsurf
+sudo apt-get install wget gpg
+wget -qO- "https://windsurf-stable.codeiumdata.com/wVxQEIWkwPUEAGf3/windsurf.gpg" | gpg --dearmor > windsurf-stable.gpg
+sudo install -D -o root -g root -m 644 windsurf-stable.gpg /etc/apt/keyrings/windsurf-stable.gpg
+echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/windsurf-stable.gpg] https://windsurf-stable.codeiumdata.com/wVxQEIWkwPUEAGf3/apt stable main" | sudo tee /etc/apt/sources.list.d/windsurf.list > /dev/null
+rm -f windsurf-stable.gpg
+sudo apt install apt-transport-https
+sudo apt update
+sudo apt install windsurf
+
+# k6
+sudo gpg -k
+sudo gpg --no-default-keyring --keyring /usr/share/keyrings/k6-archive-keyring.gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys C5AD17C747E3415A3642D57D77C6C491D6AC1D69
+echo "deb [signed-by=/usr/share/keyrings/k6-archive-keyring.gpg] https://dl.k6.io/deb stable main" | sudo tee /etc/apt/sources.list.d/k6.list
+sudo apt-get update
+sudo apt-get install k6
+
+# aws
+snap install aws-cli --classic
+cd ~alejandro/Downloads/
+wget https://github.com/aws/aws-sam-cli/releases/latest/download/aws-sam-cli-linux-x86_64.zip
+unzip aws-sam-cli-linux-x86_64.zip -d sam-installation
+sudo ./sam-installation/install
+sam --version
+npm install -g aws-cdk
